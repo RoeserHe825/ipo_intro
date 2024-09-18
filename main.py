405a636cd8a3ef5e9f70e70cfedd666f2959157f
RoeserHe825 <RoeserHe825@northwested.org>
2024-09-18 14:09:35 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+# Henry Roeser
+# Sep 18 2024
+# IPO intro
+
+num1 = int(input('Enter a number: (Example: 7)\n'))
+num2 = int(input('Enter another number: (Example: 4)\n'))
+
+total = num1 + num2
+
+print(f'First number entered was: {num1}')
+print(f'Second number entered was: {num2}')
+print()
+print(f'The sum of {num1} and {num2} is: {total}')
